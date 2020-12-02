20151940548bb4d51a04f3cba501954b819cd6a1
nabda <abdelaliboulajine@gmail.com>
2020-12-02 23:04:45 +0100

    day 2

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('data.txt', 'utf8').split('\n').map(i => parseInt(i, 10));
+const input =
+    fs.readFileSync('data.txt', 'utf8')
+        .split('\n')
+        .filter(i => i !== '')
+        .map(i => {
+            i = i.split(' ');
+            return {min: i[0].split('-')[0], max: i[0].split('-')[1], char: i[1].slice(0,1), pwd: i[2]}
+        })
+        .flat();
 
-let numberMatrix = input.map(i => input.map(x => [i, x]));
+let nbOfValidPwd1 = input.reduce((accumulator, currentValue) => {
+    let nbOfOccurrences = currentValue.pwd.split('').reduce((a,v) => {
+        if(currentValue.char === v) {
+            ++a;
+        }
+        return a;
+    }, 0);
+    if(currentValue.min <= nbOfOccurrences && nbOfOccurrences <= currentValue.max) ++accumulator;
+    return accumulator;
+}, 0);
 
-numberMatrix = numberMatrix.flat().filter(([x, y]) => x !== y);
+let nbOfValidPwd2 = input.reduce((accumulator, currentValue) => {
+    let count = 0;
+    if(currentValue.pwd.charAt(currentValue.min - 1) === currentValue.char) ++count;
+    if(currentValue.pwd.charAt(currentValue.max - 1) === currentValue.char) ++count;
+    if(count === 1) ++accumulator;
+    return accumulator;
+}, 0);
 
-const found = numberMatrix.find(([x, y]) => x+y === 2020);
+console.log("nb valid pwd, part 1", nbOfValidPwd1);
 
-console.log(found[0] * found[1]);
+
+console.log("nb valid pwd, part 2", nbOfValidPwd2);
